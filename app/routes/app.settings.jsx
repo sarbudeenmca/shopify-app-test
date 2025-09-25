cbe6042dca2140c829e34260329ec500594e9ad5
vapesbarsdistribution08-sudo <vapesbarsdistribution08@gmail.com>
2025-09-25 13:26:25 +0400

    additional renamed to settings

---
+++ app/routes/app.settings.jsx
@@ -0,0 +1,107 @@
+import {
+  Box,
+  Card,
+  Layout,
+  Link,
+  List,
+  Page,
+  Text,
+  BlockStack,
+  InlineGrid,
+  TextField,
+  Divider,
+  useBreakpoints,
+  Button,
+} from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
+import { useState } from "react";
+import { json } from "@remix-run/node";
+import { useLoaderData, Form } from "@remix-run/react";
+
+export async function loader() {
+  // provides data to the component
+  let settings = {
+    name: "Discount App",
+    description: "Discount Request form",
+  };
+
+  return json(settings);
+}
+
+export async function action({ request }) {
+  let settings = await request.formData();
+  settings = Object.fromEntries(settings);
+  return json(settings);
+}
+
+export default function SettingsPage() {
+  const settings = useLoaderData();
+  const [formState, setFormState] = useState(settings);
+  const { smUp } = useBreakpoints();
+  return (
+    <Page>
+      <TitleBar title="Settings" />
+      <BlockStack gap={{ xs: "800", sm: "400" }}>
+        <InlineGrid columns={{ xs: "1fr", md: "2fr 5fr" }} gap="400">
+          <Box
+            as="section"
+            paddingInlineStart={{ xs: 400, sm: 0 }}
+            paddingInlineEnd={{ xs: 400, sm: 0 }}
+          >
+            <BlockStack gap="400">
+              <Text as="h3" variant="headingMd">
+                Form Header
+              </Text>
+              <Text as="p" variant="bodyMd">
+                Header Title and Description
+              </Text>
+            </BlockStack>
+          </Box>
+          <Card roundedAbove="sm">
+            <Form method="POST">
+              <BlockStack gap="400">
+                <TextField
+                  label="Form Title"
+                  name="name"
+                  value={formState.name}
+                  onChange={(value) =>
+                    setFormState({ ...formState, name: value })
+                  }
+                />
+                <TextField
+                  label="Description"
+                  name="description"
+                  value={formState.description}
+                  onChange={(value) =>
+                    setFormState({ ...formState, description: value })
+                  }
+                />
+                <Button variant="primary" submit={true}>
+                  Save
+                </Button>
+              </BlockStack>
+            </Form>
+          </Card>
+        </InlineGrid>
+        {smUp ? <Divider /> : null}
+      </BlockStack>
+    </Page>
+  );
+}
+
+function Code({ children }) {
+  return (
+    <Box
+      as="span"
+      padding="025"
+      paddingInlineStart="100"
+      paddingInlineEnd="100"
+      background="bg-surface-active"
+      borderWidth="025"
+      borderColor="border"
+      borderRadius="100"
+    >
+      <code>{children}</code>
+    </Box>
+  );
+}
