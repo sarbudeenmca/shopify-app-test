52adb82189cb6b04b93e3187af849fdfec23ad51
vapesbarsdistribution08-sudo <vapesbarsdistribution08@gmail.com>
2025-09-25 19:11:16 +0400

    sqlite to postgress

--- prisma/migrations/20250925114930_add_discount_request/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "DiscountRequest" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "product" TEXT NOT NULL,
-    "quantity" INTEGER NOT NULL,
-    "price" REAL NOT NULL,
-    "comments" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
